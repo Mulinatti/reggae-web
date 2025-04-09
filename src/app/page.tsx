72df49176ec73b1b0d4e45d84d203d3e97c52d59
mulinatti <gabrielmulinari2002@gmail.com>
2025-04-09 17:56:01 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Button } from "@/src/components/ui/button";
+
+export default function Login() {
+  return (
+    <Image width={100} height={100} src="/logo.png" alt=""/>
+  );
+}
